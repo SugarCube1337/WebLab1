0503d588733e86d0e68985c01e2747f34887765d
SugarCube1337 <126420288+SugarCube1337@users.noreply.github.com>
2023-10-10 03:16:55 +0300

    async-await, server validation, css update, from delete

--- process.php
+++ process.php
@@ -6,8 +6,17 @@ if (!isset($_SESSION['results'])) {
     $_SESSION['results'] = array();
 }
 
+function isValidInput($x, $y, $r)
+{
+    return is_numeric($x) && is_numeric($y) && is_numeric($r);
+}
+
 function checkPoint($x, $y, $r)
 {
+    // Проверка валидности входных данных
+    if (!isValidInput($x, $y, $r)) {
+        return 'Invalid';
+    }
     // Проверка квадрата в левой верхней четверти
     $isInSquare = ($x <= 0 && $x >= -$r && $y >= 0 && $y <= $r);
 
@@ -15,16 +24,20 @@ function checkPoint($x, $y, $r)
     $isInCircle = ($x >= 0 && $y >= 0 && $y <= sqrt($r * $r / 4 - $x * $x));
 
     // Проверка треугольника в левой нижней четверти
-    $isInTriangle = ($x <= 0 && $y <= 0 && $y >= -($x + $r) / 2);
+    $isInTriangle = ($x <= 0 && $y <= 0 && $y >= -$x - $r / 2);
 
     // Если точка попадает хотя бы в одну из областей, возвращаем true
     return $isInSquare || $isInCircle || $isInTriangle;
 }
 
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['x']) && isset($_POST['y']) && isset($_POST['r'])) {
     $x = floatval($_POST['x']);
     $y = floatval($_POST['y']);
     $r = floatval($_POST['r']);
+
+    // Проверка валидности входных данных
+    $isValid = isValidInput($x, $y, $r);
 
     // Вычисляем результат
     $result = checkPoint($x, $y, $r);
--- script.js
+++ script.js
@@ -1,20 +1,55 @@
-document.getElementById("pointForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Предотвращаем отправку формы
-//сверху
-    const formData = new FormData(this);
+document.getElementById("checkButton").addEventListener("click", async function() {
 // async-await
-    fetch('process.php', {
-        method: 'POST',
-        body: formData
-    })
-        .then(response => response.text())
-        .then(data => {
-            // Обновляем таблицу результатов на странице
-            var resultTable = document.getElementById('resultTable');
-            resultTable.innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Ошибка:', error);
-        });
-});
 
+    const x = document.querySelector('input[name="x"]:checked');
+    const y = document.getElementById('y').value;
+    const r = document.getElementById('r').value;
+    const errorMessage = document.getElementById('error-message');
+    const successMessage = document.getElementById('success-message');
+
+    // Проверяем, введены ли все данные
+    if (!x || !y || !r) {
+        errorMessage.textContent = 'Пожалуйста, введите все данные.';
+        successMessage.textContent = '';
+        return;
+    }
+
+    // Проверяем, являются ли данные числами
+    if (isNaN(y) || isNaN(r)) {
+        errorMessage.textContent = 'Координата Y и радиус R должны быть числами.';
+        successMessage.textContent = '';
+        return;
+    }
+
+    if (y < -5 || y > 5) {
+        errorMessage.textContent = 'Координата Y должна быть в пределах от -5 до 5.';
+        successMessage.textContent = '';
+        return;
+    }
+
+    // Очищаем сообщение об ошибке, если данные введены правильно
+    errorMessage.textContent = '';
+    successMessage.textContent = 'Данные введены успешно!';
+
+    try {
+        const formData = new FormData();
+        formData.append('x', x.value);
+        formData.append('y', y);
+        formData.append('r', r);
+
+        const response = await fetch('process.php', {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            const data = await response.text();
+            const resultTable = document.getElementById('resultTable');
+            resultTable.innerHTML = data;
+        } else {
+            console.error('Ошибка сервера:', response.status);
+        }
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+});
